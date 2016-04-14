784f93fb65d10638e8483f3852391144d985d96e
Pierre Rudloff <contact@rudloff.pro>
2016-04-14 12:29:02 +0200

    Remove useless comment

--- tests/PopenStreamTest.php
+++ tests/PopenStreamTest.php
@@ -13,16 +13,4 @@ class PopenStreamTest extends PHPUnit_Framework_TestCase
         $stream = new PopenStream('echo "foo"', 'r');
         $this->assertTrue($stream->isReadable());
     }
-
-    /*public function testClose()
-    {
-        $stream = new PopenStream('ls', 'r');
-        $stream->close();
-
-        $this->assertFalse($stream->isSeekable());
-        $this->assertFalse($stream->isReadable());
-        $this->assertFalse($stream->isWritable());
-        $this->assertNull($stream->getSize());
-        $this->assertEmpty($stream->getMetadata());
-    }*/
 }
